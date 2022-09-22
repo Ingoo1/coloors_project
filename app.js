32c868587457e36ed1b7fe9d76e7f898a16de3c8
Maksym_enKO <ingool.ua@gmail.com>
2022-09-22 23:44:51 +0300

    checking contrast

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ function randomColors() {
     // Add color to the background
     div.style.backgroundColor = randomColor;
     hexText.textContent = randomColor;
+
+    checkTextContrast(randomColor, hexText);
   });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+
+  if (luminance > 0.5) {
+    text.style.color = 'black';
+  } else {
+    text.style.color = 'white';
+  }
 }
 randomColors();
